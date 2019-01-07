f2bdd05fdf49c8c18955a9246f435c95136f8674
chenyijiang <chenyijiang@Ctrip.com>
2019-01-07 20:08:16 +0800

    Add test.

--- src/main/java/com/jcy/jenkins/demo/Application.java
+++ src/main/java/com/jcy/jenkins/demo/Application.java
@@ -2,10 +2,19 @@ package com.jcy.jenkins.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/test/java/com/jcy/jenkins/demo/ClickMoreTest.java
@@ -0,0 +1,40 @@
+package com.jcy.jenkins.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.util.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class ClickMoreTest {
+    public static void main(String[] args)
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        //Puts a Implicit wait, Will wait for 10 seconds before throwing exception
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Launch website
+        driver.get("http://localhost:8089/");
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+
+        int beforeSize = driver.findElements(By.className("msg")).size();
+
+        // Click Calculate Button
+        driver.findElement(By.id("moreBtn")).click();
+
+        // Get the Result Text based on its xpath
+        int afterSize = driver.findElements(By.className("msg")).size();
+
+        Assert.isTrue(beforeSize + 5 == afterSize, "Fail.");
+
+        //Close the Browser.
+        driver.close();
+    }
+}
