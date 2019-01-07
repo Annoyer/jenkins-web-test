df7f07aad3ba7eb543178bb8624cc52f316e59c0
chenyijiang <chenyijiang@Ctrip.com>
2019-01-07 13:51:18 +0800

    init

---
+++ src/main/java/com/jcy/jenkins/demo/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.jcy.jenkins.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class TestController {
+
+    private static List<String> list = new ArrayList<>();
+
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value="/more")
+    public String more(Model model) {
+        long current = System.currentTimeMillis();
+        for (int i=0; i<5; i++) {
+            list.add(current + "-more-" + i);
+        }
+
+        model.addAttribute("msgs", list);
+        return "index::msgTable";
+
+    }
+
+}
