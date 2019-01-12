3527a0a94706e65389f27d87add617404f328bf8
Annoyer <247221925@qq.com>
2019-01-12 17:41:24 +0800

    Commit 4.

--- src/main/java/com/jcy/jenkins/demo/controller/TestController.java
+++ src/main/java/com/jcy/jenkins/demo/controller/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     private static List<String> list = new ArrayList<>();
 
-    @RequestMapping(value="/3")
+    @RequestMapping(value="/")
     public String index() {
         return "index";
     }
